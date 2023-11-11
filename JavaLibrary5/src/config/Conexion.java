d84833486fd364de2978f4de8978ec8dd4ba5ede
AndersonHuayta <anderson.huayta@vallegrande.edu.pe>
2023-11-11 01:34:07 -0500

    CrudPractica

---
+++ JavaLibrary5/src/config/Conexion.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package config;
+
+/**
+ *
+ * @author ander
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    Connection con;
+
+    public Conexion() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/general?characterEncoding=UTF-8", "root", "admin");
+
+        } catch (Exception e) {
+            System.out.println("Error conexion " + e);
+        }
+    }
+    
+    public Connection getConnection(){
+        return con;
+    }
+}
